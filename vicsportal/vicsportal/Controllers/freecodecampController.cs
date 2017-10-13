739378a73b83288d10a6e77a9a10d4bcb9b168d5
DESKTOP-G7JTIU0\victor <vmdlssnt@gmail.com>
2017-10-12 16:12:26 -0800

    integrating freecodecamp front end projects

---
+++ vicsportal/vicsportal/Controllers/freecodecampController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vicsportal.Controllers
+{
+    public class freecodecampController : Controller
+    {
+        // GET: freecodecamp
+        public ActionResult RandomQuoteMachine()
+        {
+            return View();
+        }
+
+        public ActionResult LocalWeather()
+        {
+            return View();
+        }
+
+        public ActionResult WikipediaViewer()
+        {
+
+            return View();
+            
+        }
+
+        public ActionResult TwitchTVAPI()
+        {
+            return View();
+        }
+
+        public ActionResult JavascriptCalculator()
+        {
+            return View();
+        }
+
+        public ActionResult PomodoroClock()
+        {
+            return View();
+        }
+    }
+}
