b3cb9a4e36d472698e4a3da6329f7f837f5a0cae
DESKTOP-G7JTIU0\victor <vmdlssnt@gmail.com>
2017-10-31 01:42:24 -0800

    first commit of react Camper Leaderboard FCC project

---
+++ vicsportal/vicsportal/Scripts/Camper Leaderboard/ClientApp/components/UsersTable.js
@@ -0,0 +1,45 @@
+﻿import React from 'react';
+
+const UsersTable = (data) => {
+    let users = data.users
+    console.log(users);
+    if (users==="") {
+        return <div>Loading...</div>
+    }
+
+    return (
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>All time</th>
+                </tr>
+            </thead>
+            {
+                React.createElement("tbody",
+                    null,
+                    users.map(function (element) {
+                        return (
+
+                            <tr>
+                                <td>
+                                    <p>{element.username} </p>
+                                </td>
+                                <td>
+                                    <p>{element.alltime}</p>
+                                </td>
+                            </tr>
+
+
+
+                        )
+                    }))
+
+            }
+        </table>
+
+        )
+}
+
+export default UsersTable
---
+++ vicsportal/vicsportal/Scripts/Camper Leaderboard/ClientApp/main.js
@@ -0,0 +1,44 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import UsersTable from './components/UsersTable';
+
+class CamperLeaderboard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: ''
+            
+        }
+        this.getdata();
+    }
+    getdata() {
+        var thisobject = this;
+        
+        axios.get("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
+        .then(function(response) {
+                thisobject.setState({ users: response.data },
+                function() {
+                    console.log(thisobject.state.users)                
+                })
+            })
+
+
+    }
+    render() {
+        return (
+            <div>
+
+                
+                    <UsersTable users={this.state.users}></UsersTable>
+                
+            </div>
+        );    
+    }
+    
+    
+}
+
+ReactDOM.render(<CamperLeaderboard />, document.querySelector(".myapp"));
+
+
