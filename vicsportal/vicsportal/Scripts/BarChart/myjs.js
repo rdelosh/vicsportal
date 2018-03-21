fd3a9e21c2df590065f9876686bcedbd2e7beaf4
DESKTOP-G7JTIU0\victor <vmdlssnt@gmail.com>
2018-03-21 12:23:11 -0800

    VISUALIZE BARCHART: created boilerplate for BarChart

---
+++ vicsportal/vicsportal/Scripts/BarChart/myjs.js
@@ -0,0 +1,19 @@
+var WIDTH=1200,
+	HEIGHT=800
+
+var svg = d3.select('body')
+	.append('svg')
+		.attr("width",WIDTH)
+		.attr("height",HEIGHT)
+	.append('g')
+
+
+
+
+
+
+
+// d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json",function(data){
+// 	console.log("asd")
+// })
+
